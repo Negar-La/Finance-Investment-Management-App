d2272d5f6ef95ebabeaec2b89d64c22b7bcf889b
Negar Lashgari <negar.lashgari@gmail.com>
2023-07-24 12:05:34 -0400

    adding entities

---
+++ src/main/java/Final/project/entities/Transaction.java
@@ -0,0 +1,103 @@
+package Final.project.entities;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class Transaction {
+    private int transactionID;
+    private LocalDateTime transactionDate;
+    private BigDecimal amount;
+    private String transactionType;
+    private String description;
+
+    private Portfolio portfolio;
+    private Asset asset;
+
+    // Constructors, getters, setters, and other methods
+
+    public int getTransactionID() {
+        return transactionID;
+    }
+
+    public void setTransactionID(int transactionID) {
+        this.transactionID = transactionID;
+    }
+
+    public LocalDateTime getTransactionDate() {
+        return transactionDate;
+    }
+
+    public void setTransactionDate(LocalDateTime transactionDate) {
+        this.transactionDate = transactionDate;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Portfolio getPortfolio() {
+        return portfolio;
+    }
+
+    public void setPortfolio(Portfolio portfolio) {
+        this.portfolio = portfolio;
+    }
+
+    public Asset getAsset() {
+        return asset;
+    }
+
+    public void setAsset(Asset asset) {
+        this.asset = asset;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Transaction that = (Transaction) o;
+
+        if (transactionID != that.transactionID) return false;
+        if (!transactionDate.equals(that.transactionDate)) return false;
+        if (!amount.equals(that.amount)) return false;
+        if (!transactionType.equals(that.transactionType)) return false;
+        if (!description.equals(that.description)) return false;
+        if (!portfolio.equals(that.portfolio)) return false;
+        return asset.equals(that.asset);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = transactionID;
+        result = 31 * result + transactionDate.hashCode();
+        result = 31 * result + amount.hashCode();
+        result = 31 * result + transactionType.hashCode();
+        result = 31 * result + description.hashCode();
+        result = 31 * result + portfolio.hashCode();
+        result = 31 * result + asset.hashCode();
+        return result;
+    }
+}
+
---
+++ src/main/java/Final/project/entities/User.java
@@ -0,0 +1,89 @@
+package Final.project.entities;
+
+import java.util.List;
+
+public class User {
+    private int userID;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private List<Account> accounts; // Composition: User has multiple Accounts
+
+    // Constructor, getters, setters, and other methods
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(List<Account> accounts) {
+        this.accounts = accounts;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (userID != user.userID) return false;
+        if (!firstName.equals(user.firstName)) return false;
+        if (!lastName.equals(user.lastName)) return false;
+        if (!email.equals(user.email)) return false;
+        if (!phone.equals(user.phone)) return false;
+        return accounts.equals(user.accounts);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = userID;
+        result = 31 * result + firstName.hashCode();
+        result = 31 * result + lastName.hashCode();
+        result = 31 * result + email.hashCode();
+        result = 31 * result + phone.hashCode();
+        result = 31 * result + accounts.hashCode();
+        return result;
+    }
+}
+
